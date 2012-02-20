7b3580e18c96f94283d10affa8109dbcd952701a
Olivier Abdesselam <yazgoo+gitorious@free.fr>
2012-02-21 00:34:52 +0100

    fixing ! help

--- simpleirc.rb
+++ simpleirc.rb
@@ -33,16 +33,16 @@ class Command
         @commands = commands
     end
     def self.help_summary
-        puts "| This is a (very) simple didactic client for IRC:",
+        ["| This is a (very) simple didactic client for IRC:",
             "| * type «!» if you need to see this message again",
             "| * First you may wanna issue a «NICK», then a «USER»",
             "| * Autocompletion is available for command names,",
             "|   Prefixing those with #{Mark} will give you a brief help.",
             "| * Response to PING (PONG) is automated, so don't bother.",
-            "| * JOIN to join a chan, PRIVMSG to talk"
+            "| * JOIN to join a chan, PRIVMSG to talk"].join "\r\n"
     end
     def self.help name
-        if name.chompt.empty then @help_summary
+        if name.chomp.empty? then help_summary
         else @commands.has_key?(name) ? @commands[name] : name + ": not found"
         end
     end
@@ -69,16 +69,17 @@ if ARGV.size != 2
     puts "Usage: <server host name> <non encrypted port>"
     exit
 end
-Command.help_summary
-s = TCPSocket.new ARGV[0], ARGV[1]
-Thread.new do
-    while line = s.gets 
-        puts line 
-        s.puts "PONG uranus" if line.start_with?("PING ")
+puts Command.help_summary
+TCPSocket.open(ARGV[0], ARGV[1]) do |s|
+    Thread.new do
+        while line = s.gets 
+            puts line 
+            s.puts "PONG uranus" if line.start_with?("PING ")
+        end
     end
-end
-while line = Readline.readline('', true)
-    args = line.split
-    if line.start_with? Mark then puts Command.help args[0][1..-1]
-    else s.puts line end
+    while line = Readline.readline('', true)
+        args = line.split
+        if line.start_with? Mark then puts Command.help args[0][1..-1]
+        else s.puts line end
+    end
 end
